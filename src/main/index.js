93cf55cc48766ff529b7e31adedcbba9db1a4669
Drew Royster <drew.124@gmail.com>
2018-10-26 16:45:40 -0600

    adding auto updating

--- src/main/index.js
+++ src/main/index.js
@@ -308,14 +308,12 @@ const createNewFolders = async (rootFolder, folders) => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
 import { autoUpdater } from 'electron-updater'
 
 autoUpdater.on('update-downloaded', () => {
   autoUpdater.quitAndInstall()
-})
+});
 
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
+});
